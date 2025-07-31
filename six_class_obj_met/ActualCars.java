160febca7de1dae8a98254caea318bf4be5ea4bb
Sureshsai09 <sureshsai6666@gmail.com>
2025-07-31 11:10:57 +0530

    oops applications

---
+++ six_class_obj_met/ActualCars.java
@@ -0,0 +1,22 @@
+package six_class_obj_met;
+
+public class ActualCars {
+    public static void main(String[] args) {
+    
+        CarFactory cretaCar=new CarFactory();
+        cretaCar.carBrand="Hyundai";
+        cretaCar.carPrice=20.50;
+        cretaCar.carReviews=410;
+        cretaCar.carRating=4.6;
+        cretaCar.displayCar_shortInfo();
+        cretaCar.displayCar_completeInfo();
+
+        CarFactory scarpioCar=new CarFactory();
+        scarpioCar.carBrand="Mahindra";
+        scarpioCar.carPrice=17.71;
+        scarpioCar.carReviews=490;
+        scarpioCar.carRating=4.7;
+        scarpioCar.displayCar_shortInfo();
+        scarpioCar.displayCar_completeInfo();
+}
+}
---
+++ six_class_obj_met/CarFactory.java
@@ -0,0 +1,32 @@
+package six_class_obj_met;
+
+public class CarFactory {
+      //data 
+      String carBrand;
+      double carPrice;
+      int carReviews;
+      double carRating;
+
+
+
+
+
+
+//behaviour method task 
+public void displayCar_shortInfo(){
+    System.out.println("when hovered: ");
+    System.out.println("car Brand:"+carBrand);
+    System.out.println("car price:"+carPrice);
+
+}
+
+public void displayCar_completeInfo(){
+    System.out.println("car Brand:"+carBrand);
+    System.out.println("car price:"+carPrice);
+    System.out.println("when clickled: ");
+    displayCar_shortInfo();//reusing
+    System.out.println("car reviews:"+carReviews);
+    System.out.println("car rating:"+carRating);
+}
+
+}
